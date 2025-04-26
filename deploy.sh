5a0ceba315be0a4c60a4db3be85cdf0b5c8911ac
John Walley <ThatOrJohn@users.noreply.github.com>
2025-04-26 17:30:12 -0500

    fix deploy.sh push

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,6 @@ hugo --minify || { echo "Build failed"; exit 1; }
 
 # Deploy to gh-pages
 echo "Deploying to gh-pages..."
-git subtree push --prefix public origin gh-pages --force || { echo "Deploy failed"; exit 1; }
+git subtree push --prefix public origin gh-pages || { echo "Deploy failed"; exit 1; }
 
 echo "Deployed successfully!"
